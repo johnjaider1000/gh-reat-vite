62e646c9e2160d7f2b2c0aa0ff48242807cd3e16
johnjaider1000 <jhonjaider100015@gmail.com>
2022-10-11 18:44:00 -0500

    Update url

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://johnjaider1000.github.io/gh-reat-vite/",
+  base: "https://johnjaider1000.github.io/gh-reat-vite-test/",
   plugins: [react()],
 });
