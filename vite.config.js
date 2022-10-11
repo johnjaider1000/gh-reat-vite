74f76f13c6a44b4d74109287b466afd684225e50
johnjaider1000 <jhonjaider100015@gmail.com>
2022-10-11 10:37:13 -0500

    fix bug

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://johnjaider1000.github.io/",
+  base: "https://johnjaider1000.github.io/gh-reat-vite/",
   plugins: [react()],
 });
